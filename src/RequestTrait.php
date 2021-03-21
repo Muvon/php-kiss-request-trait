2227e0c47f88b74b3c8e1e8a22912fd483d7d7d8
Don Hardman <don.hardman@muvon.io>
2021-03-21 16:26:33 +0700

    Add multi curl logic and simple test to check it

--- src/RequestTrait.php
+++ src/RequestTrait.php
@@ -1,15 +1,39 @@
 <?php
 namespace Muvon\KISS;
 
+use Error;
 use Throwable;
+use CurlHandle;
+use CurlMultiHandle;
 
 trait RequestTrait {
   protected int $request_connect_timeout = 5;
   protected int $request_timeout = 12;
   protected int $request_keepalive = 20;
   protected bool $request_json = true;
+  protected array $request_handlers = [];
+  protected ?CurlMultiHandle $request_mh = null;
 
-  protected function request(string $url, array $payload = [], string $method = 'POST', array $headers = []): array {
+  /**
+   * Run multi model
+   *
+   * @return self
+   */
+  protected function multi(): self {
+    $this->request_mh = curl_multi_init();
+    return $this;
+  }
+
+  /**
+   * Do single or multi request if multi() caleld before
+   *
+   * @param string $url
+   * @param array $payload
+   * @param string $method Can be POST or GET only
+   * @param array $headers Array with headers. Each entry as string
+   * @return self|array in case multi() mode reqturns self otherswise array
+   */
+  protected function request(string $url, array $payload = [], string $method = 'POST', array $headers = []): self|array {
     $get_params = [];
     if ($method === 'GET') {
       $get_params = array_merge($get_params, $payload);
@@ -32,6 +56,49 @@ trait RequestTrait {
       curl_setopt($ch, CURLOPT_POST, 1);
       curl_setopt($ch, CURLOPT_POSTFIELDS, $this->request_json ? json_encode($payload) : http_build_query($payload, false, '&'));
     }
+    if ($this->request_mh) {
+      $this->request_handlers[] = $ch;
+      curl_multi_add_handle($this->request_mh, $ch);
+      return $this;
+    }
+
+    return $this->process($ch);
+  }
+
+  /**
+   * This method execute multiple request in multi() mode
+   * If we call this methods without multi it throws Exception
+   * In case if one or more responses failed it throws Exception
+   *
+   * @return array
+   */
+  protected function exec(): array {
+    if (!$this->request_mh) {
+      throw new Error('Trying to exec request that ws not inited');
+    }
+    do {
+      $status = curl_multi_exec($this->request_mh, $active);
+      if ($active) {
+        curl_multi_select($this->request_mh);
+      }
+    } while ($active && $status == CURLM_OK);
+
+    $result = [];
+    foreach ($this->request_handlers as $ch) {
+      [$err, $resp] = $this->process($ch);
+      if ($err) {
+        throw new Error('One of the requests has response error: ' . $err);
+      }
+      $result[] = $resp;
+    }
+    curl_multi_close($this->request_mh);
+    unset($this->request_handlers);
+    $this->request_mh = null;
+
+    return $result;
+  }
+
+  private function process(CurlHandle $ch): array {
     try {
       $err_code = curl_error($ch);
       if ($err_code) {
@@ -46,7 +113,8 @@ trait RequestTrait {
         return [$err, false];
       }
 
-      $response = curl_exec($ch);
+      $fetch_fn = $this->request_mh ? 'curl_multi_getcontent' : 'curl_exec';
+      $response = $fetch_fn($ch);
       $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
       curl_close($ch);
       if (($httpcode !== 200 && $httpcode !== 201)) {
@@ -60,6 +128,10 @@ trait RequestTrait {
       return [null, $this->request_json ? json_decode($response, true) : $response];
     } catch (Throwable $T) {
       return ['e_request_failed', $T->getMessage()];
+    } finally {
+      if ($this->request_mh) {
+        curl_multi_remove_handle($this->request_mh, $ch);
+      }
     }
   }
 }
--- test/RequestTest.php
+++ test/RequestTest.php
@@ -24,6 +24,14 @@ final class Request {
   public function run(string $url, array $payload = [], string $method = 'POST') {
     return $this->request($url, $payload, $method);
   }
+
+  public function runMulti(array $requests): array {
+    $this->multi();
+    foreach ($requests as $request) {
+      $this->request(...$request);
+    }
+    return $this->exec();
+  }
 }
 
 final class RequestTest extends TestCase {
@@ -50,5 +58,17 @@ final class RequestTest extends TestCase {
     $this->Client->setRequestJson(false);
     [$err, $res] = $this->Client->run('https://google.com', [], 'GET');
     $this->assertEquals('e_request_failed', $err);
+  }
+
+  public function testRequestMultiSucceed() {
+    $this->Client->setRequestJson(false);
+    $responses = $this->Client->runMulti([
+      ['https://www.google.com', [], 'GET'],
+      ['https://yandex.ru', [], 'GET'],
+    ]);
+
+    $this->assertIsScalar($responses[0]);
+    $this->assertIsString($responses[1]);
+    $this->assertNotEquals($responses[0], $responses[1]);
   }
 }
